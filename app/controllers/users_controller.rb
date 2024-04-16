5b9d194b1b332b2942223e3ac5db80a419b7a390
keit1722 <keit1722@gmail.com>
2024-04-17 01:38:41 +0900

    新規登録処理をturbo stream化

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to @user, notice: "User was successfully created."
+      flash.now.notice = "User was successfully created."
     else
       render :new, status: :unprocessable_entity
     end
